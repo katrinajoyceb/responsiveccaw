5a6b64d3afad1c639bacd739dd8d71b6564c7fb8
Katrina Bueno <kbueno@slnzero.local>
2020-01-03 15:03:48 -0600

    responsive slider section + modified button behavior in portfolio section

--- src/app/wireframes/wireframes.component.ts
+++ src/app/wireframes/wireframes.component.ts
@@ -10,6 +10,43 @@ export class WireframesComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    this.onLoad();
+  }
+
+
+
+  onLoad(){
+    var isResizing = false,
+    lastDownX = 0;
+    
+    (function() {
+    var container = document.getElementById("slider"),
+        left = document.getElementById("left_panel"),
+        right = document.getElementById("right_panel"),
+        handle = document.getElementById("drag");
+    
+    handle.onmousedown = function(e) {
+        isResizing = true;
+        lastDownX = e.clientX;
+    };
+    
+    document.onmousemove = function(e) {
+        // we don't want to do anything if we aren't resizing.
+        if (!isResizing) {
+            return;
+        }
+    
+        var offsetRight = container.clientWidth - (e.clientX - container.offsetLeft);
+    
+        left.style.right = offsetRight + "px"; 
+        right.style.width = offsetRight + "px"; 
+    }
+    
+    document.onmouseup = function(e) {
+        // stop resizing
+        isResizing = false;
+    }
+    })();
   }
 
 }
